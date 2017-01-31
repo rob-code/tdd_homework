d4e5351ac8cc4d53e9c34364aa3193a45579a841
Robert Brice <rob.a.brice@gmail.comC>
2017-01-31 15:55:04 +0000

    Functions/TDD homework files

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,61 @@
+require ('date')
+
+def return_10()
+  return 10
+end
+
+def add(a,b)
+  return a + b
+end
+
+def subtract(a,b)
+  return a - b
+end
+
+def multiply(a, b)
+  return a * b
+end
+
+def divide(a, b)
+  return a / b
+end
+
+def length_of_string(string)
+  string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(a, b)
+  return a.to_f + b.to_f
+end
+
+def number_to_full_month_name(month_number)
+ return Date::MONTHNAMES[month_number]
+end
+
+def number_to_short_month_name(month_number)
+  month_name = Date::MONTHNAMES[month_number]
+  return month_name[0..2]
+end
+
+def volume_of_cube(a, b, c)
+  return a * b * c
+end
+
+def volume_of_sphere(radius)
+  return (4 / 3) * Math::PI * radius ** 2
+end
+
+def fahrenheit_to_celsius(temp_f)
+  return ((temp_f - 32) * 5) / 9
+end
+
+
+
+puts fahrenheit_to_celsius(250)
+
+
+
